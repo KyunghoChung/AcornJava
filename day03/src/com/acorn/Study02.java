57dd75d0e753e612053c3419fe9c03726c8979d2
CKH <khchung30630@gmail.com>
2023-08-02 11:22:31 +0900

    [ADD]Study01&02 실습추가

---
+++ day03/src/com/acorn/Study02.java
@@ -0,0 +1,21 @@
+package com.acorn;
+
+import java.util.Scanner;
+public class Study02 {
+
+    public static void main(String[] args) {
+        // 키보드 입력 개체 생성
+        Scanner sc = new Scanner(System.in);
+        // 두 개의 숫자를 키보드 입력
+
+        // 두 개의 난수를 생성
+        int iRanNum01 = (int)((Math.random()*100))+1;
+        int iRanNum02 = (int)((Math.random()*100))+1;
+
+        // 우승 조건: 두 개의 수의 차가 가장 적은 수
+
+        // 우승 여부 출력
+    }
+
+}
+
