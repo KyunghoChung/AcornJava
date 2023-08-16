82bd411fbf41a8b3701a59a973d0cb882e84f5ab
CKH <khchung30630@gmail.com>
2023-08-16 09:12:14 +0900

    [ADD]강사님ReviewMain01 오류없도록 주석처리

--- Java0811/src/day10/professor/ReviewMain01.java
+++ Java0811/src/day10/professor/ReviewMain01.java
@@ -11,9 +11,9 @@ public class ReviewMain01 {
         // 선언된 세 개의 변수 중 두번째 변수에 임의의 값을 할당
         b = 10;
         // 세 개의 변수를 출력
-        System.out.println(a);
+        // System.out.println(a);
         System.out.println(b);
-        System.out.println(c);
+        // System.out.println(c);
 
     }
 }
