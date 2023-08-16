9f7ee5b18d8cee6ddcf2ba959c8f50b12fbe5261
CKH <khchung30630@gmail.com>
2023-08-16 17:51:17 +0900

    [MOD]BusinessMain01 수정

--- Java0816/src/acorn/java0816/BusinessMain01.java
+++ Java0816/src/acorn/java0816/BusinessMain01.java
@@ -15,8 +15,8 @@ public class BusinessMain01 {
         // 반복문(10)
         for(int i = 0; i < 10; ++i) {
             // 사용자 주문 처리 함수
-            iCurrentStock1 = goodStockProcess(iCurrentStock1, iProperStock1);
-            iCurrentStock2 = goodStockProcess(iCurrentStock2, iProperStock2);
+            iCurrentStock1 = goodStockProcess(iCurrentStock1, iProperStock1, sc);
+            iCurrentStock2 = goodStockProcess(iCurrentStock2, iProperStock2, sc);
 
             // 입력대기(Thread.sleep(1000))
             System.out.println("계속하려면 아무키나 누르세요...");
@@ -30,7 +30,7 @@ public class BusinessMain01 {
         sc.close();
     }
 
-    public static int goodStockProcess(int iCurrentStock, int iProperStock) {
+    public static int goodStockProcess(int iCurrentStock, int iProperStock, Scanner sc) {
         // 현재재고와 적정재고 확인
         // 현재재고가 적정재고보다 작은지
         if(iCurrentStock < iProperStock) {
@@ -39,14 +39,26 @@ public class BusinessMain01 {
             iCurrentStock += (iProperStock - iCurrentStock);
         }
 
+        System.out.print("주문하시겠습니까?>>>(y/n) ");
+        String sInputValue = sc.nextLine();
+        if(sInputValue.equalsIgnoreCase("y")){
+            System.out.println("주문량을 입력하세요: ");
+            int iInputValue = sc.nextInt();
+            userOrderProcess(iInputValue);
+        }
         // 사용자 주문 시뮬레이션(랜덤을 사용)
-        int iCustomerOrderQty = (int)(Math.random()*iProperStock);
+//        int iCustomerOrderQty = (int)(Math.random()*iProperStock);
         // 현재재고 - 사용자주문량
-        iCurrentStock -= iCustomerOrderQty;
+//        iCurrentStock -= iCustomerOrderQty;
         // 출력: 적정재고, 현재재고
         System.out.println("적정재고:" + iProperStock + "현재재고:" + iCurrentStock);
 
         return iCurrentStock;
+    }
+
+    public static void userOrderProcess(int iInputValue){
+//        // 현재재고 - 사용자주문량
+//        iCurrentStock -= iCustomerOrderQty;
     }
 
 }
