66b4a3dc72a5485640bb21cb7c833dd4d8e24042
CKH <khchung30630@gmail.com>
2023-08-18 14:16:30 +0900

    [ADD]강사님 Override 코드

--- Java0818/src/com/acorn/inheritance/professor/AudioChild.java
+++ Java0818/src/com/acorn/inheritance/professor/AudioChild.java
@@ -1,2 +1,15 @@
-package com.acorn.inheritance.professor;public class AudioChild {
+package com.acorn.inheritance.professor;
+
+public class AudioChild extends AudioTVParent {
+
+    public AudioChild() { }
+
+    public AudioChild(boolean power, int volumn) {
+        super(power, volumn);
+    }
+
+    public void tune() {
+        String sOnOff = getPower() ?  "ha ha ha..." : "turn it on";
+        System.out.println(sOnOff);
+    }
 }
