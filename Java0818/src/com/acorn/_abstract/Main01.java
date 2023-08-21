77aaf6ff762f3bf8e6889408621fb02f064f90a3
CKH <khchung30630@gmail.com>
2023-08-21 09:10:05 +0900

    [ADD]상속개념 록지파일

---
+++ Java0818/src/com/acorn/_abstract/Main01.java
@@ -0,0 +1,11 @@
+package com.acorn._abstract;
+
+public class Main01 {
+
+    public static void main(String[] args) {
+        Shape shape = new Circle(5.0, "원");
+        shape.draw();
+
+    }
+
+}
